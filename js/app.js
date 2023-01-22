003567ec66c2e58fb313dff3ecf27f7bfe27c2fb
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-22 11:59:26 +0200

    Add function to change read state

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-// Array to store books 
-let myLibrary = [];
+// Array to store books
+const myLibrary = [];
 
-function Book(author='', title='', pagesNum='', read='not read yet') {
+function Book(author = "", title = "", pagesNum = "", read = "not read yet") {
   this.author = author;
   this.title = title;
   this.pagesNum = pagesNum;
   this.read = read;
 }
+
+Book.prototype.readStatus = function () {
+  if (this.read === "not read yet") {
+    this.read = "read";
+  } else {
+    this.read = "not read yet";
+  }
+};

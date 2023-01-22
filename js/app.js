27748cdaf7e0989a16a9e5ef5f86b70cfb73ec43
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-22 11:44:38 +0200

    Add constructor for creating objects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 // Array to store books 
 let myLibrary = [];
 
+function Book(author='', title='', pagesNum='', read='not read yet') {
+  this.author = author;
+  this.title = title;
+  this.pagesNum = pagesNum;
+  this.read = read;
+}

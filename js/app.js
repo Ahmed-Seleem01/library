f412aa923abf7068352d24260fcfdc4f85975ab0
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-22 16:29:22 +0200

    Refactor the code   comment console logs and fix function names

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 // Array to store books
 const myLibrary = [];
 
-function Book(author = "", title = "", pagesNum = "", read = "not read yet") {
+function Book(author = "", title = "", pagesNum = "", read = "Not read yet") {
   this.author = author;
   this.title = title;
   this.pagesNum = pagesNum;
   this.read = read;
 }
 
-Book.prototype.readStatus = function () {
-  if (this.read === "not read yet") {
-    this.read = "read";
+Book.prototype.readStatus = function readStatus () {
+  // console.log(this)
+  if (this.read === "Not read yet") {
+    this.read = "Read";
   } else {
-    this.read = "not read yet";
+    this.read = "Not read yet";
   }
   return this.read;
 };
@@ -29,23 +30,23 @@ function createBtn(name) {
 }
 
 function removeElms(e) {
-    const parentElm = e.target.parentElement.parentElement;
-    const elmIndex = parentElm.getAttribute('data-index');
-    myLibrary.splice(elmIndex, 1);
-    showBooks(myLibrary);
+  const parentElm = e.target.parentElement.parentElement;
+  const elmIndex = parentElm.getAttribute("data-index");
+  myLibrary.splice(elmIndex, 1);
+  showBooks(myLibrary);
 }
 
-function readCondition(e){
-    const parentElm = e.target.parentElement.parentElement;
-    const elmIndex = parentElm.getAttribute('data-index');
-    e.target.textContent =  myLibrary[elmIndex].readStatus();
+function readCondition(e) {
+  const parentElm = e.target.parentElement.parentElement;
+  const elmIndex = parentElm.getAttribute("data-index");
+  e.target.textContent = myLibrary[elmIndex].readStatus();
 }
 
-function addButtons(){
+function addButtons() {
   const removeBtn = createBtn("Remove");
   const readBtn = createBtn("Read");
-  removeBtn.addEventListener('click', removeElms);
-  readBtn.addEventListener('click', readCondition)
+  removeBtn.addEventListener("click", removeElms);
+  readBtn.addEventListener("click", readCondition);
   const btnContainer = document.createElement("div");
   btnContainer.appendChild(removeBtn);
   btnContainer.appendChild(readBtn);
@@ -57,11 +58,12 @@ function showBooks(arr) {
   container.innerHTML = "";
   for (let i = 0; i < arr.length; i++) {
     const bookDiv = document.createElement("div");
-    bookDiv.innerText = `Book: ${arr[i].title} 
+    bookDiv.innerText = `Title: ${arr[i].title} 
     Author: ${arr[i].author} 
     Pages: ${arr[i].pagesNum}`;
     bookDiv.setAttribute("data-index", i);
     const btnContainer = addButtons();
+    btnContainer.querySelector(".read").textContent = arr[i].read;
     bookDiv.appendChild(btnContainer);
     container.appendChild(bookDiv);
   }
@@ -77,9 +79,8 @@ submitBtn.addEventListener("click", (e) => {
     formElements[2].value,
     formElements[3].value
   );
-  //console.log(formElements[3].value)
+  // console.log(formElements[3].value);
   addBookToLibrary(book);
   showBooks(myLibrary);
-  console.log(myLibrary);
+  // console.log(myLibrary);
 });
-
